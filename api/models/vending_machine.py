b1df34546a75d4d5f217b2f790f1166e9f59493c
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-02-04 22:00:21 +0700

    remove unused code

--- api/models/vending_machine.py
+++ api/models/vending_machine.py
@@ -6,6 +6,3 @@ class VendingMachine(models.Model):
     name = models.CharField(max_length=100, unique=True)
     location = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
