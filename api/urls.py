51390cc3b24a00c65f43ddf9b775c32a0dd1ad9e
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-02-04 23:25:05 +0700

    fix filename

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 
-from api.views.product_view_set import ProductView
-from api.views.stock_view_set import StockView
-from api.views.vending_machine_view_set import VendingMachineView
+from api.views.product_view import ProductView
+from api.views.stock_view import StockView
+from api.views.vending_machine_view import VendingMachineView
 
 router: DefaultRouter = routers.DefaultRouter()
 router.register(r'vending-machine', VendingMachineView)
