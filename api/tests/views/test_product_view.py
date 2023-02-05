92b4d5e09d508ed785e0b51597f652ed1dc9b804
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-02-05 15:54:04 +0700

    add ci

--- api/tests/views/test_product_view.py
+++ api/tests/views/test_product_view.py
@@ -5,18 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 
 from api.models.product import Product
-
-
-def save_product() -> Product:
-    """
-    Save product.
-
-    Returns:
-        Product: Saved product.
-    """
-    product: Product = Product(id=1, name=secrets.token_hex(16), cost="{:.2f}".format(secrets.randbelow(1000)))
-    product.save()
-    return product
+from api.tests.utils import get_values, save_product
 
 
 class TestProductView(TestCase):
@@ -30,7 +19,7 @@ class TestProductView(TestCase):
             "name": secrets.token_hex(16),
             "cost": "{:.2f}".format(secrets.randbelow(1000)),
         }
-        response = self.client.post(self.path, data=new_product, content_type="application/json")
+        response: Any = self.client.post(self.path, data=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data["name"], new_product["name"])
         self.assertEqual(response.data["cost"], new_product["cost"])
@@ -38,7 +27,7 @@ class TestProductView(TestCase):
     def test_create_product_should_fail_when_name_is_null(self) -> None:
         """Test create product with invalid request where name is null."""
         new_product: dict[str, Any] = {"cost": "{:.2f}".format(secrets.randbelow(1000))}
-        response = self.client.post(self.path, data=new_product, content_type="application/json")
+        response: Any = self.client.post(self.path, data=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_product_should_fail_when_name_is_duplicate(self) -> None:
@@ -48,35 +37,34 @@ class TestProductView(TestCase):
             "name": saved_product.name,
             "cost": "{:.2f}".format(secrets.randbelow(1000)),
         }
-        response = self.client.post(self.path, data=new_product, content_type="application/json")
+        response: Any = self.client.post(self.path, data=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_product_should_fail_when_cost_is_null(self) -> None:
         """Test create product with invalid request where cost is null."""
         new_product: dict[str, Any] = {"name": secrets.token_hex(16)}
-        response = self.client.post(self.path, data=new_product, content_type="application/json")
+        response: Any = self.client.post(self.path, data=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_list_product_should_pass(self) -> None:
         """Test list product with valid request."""
         saved_product: Product = save_product()
-        response = self.client.get(self.path)
+        response: Any = self.client.get(self.path)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]["name"], saved_product.name)
-        self.assertEqual(response.data[0]["cost"], saved_product.cost)
+        self.assertIn(saved_product.name, get_values(response, "name"))
+        self.assertIn(saved_product.cost, get_values(response, "cost"))
 
     def test_retrieve_product_should_pass(self) -> None:
         """Test retrieve product with valid request."""
         saved_product: Product = save_product()
-        response = self.client.get(f"{self.path}{saved_product.id}/")
+        response: Any = self.client.get(f"{self.path}{saved_product.id}/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["name"], saved_product.name)
         self.assertEqual(response.data["cost"], saved_product.cost)
 
     def test_retrieve_product_should_fail_when_id_is_not_found(self) -> None:
         """Test retrieve product with invalid request where product id does not exist."""
-        response = self.client.get(f"{self.path}1/")
+        response: Any = self.client.get(f"{self.path}99999/")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_product_should_pass(self) -> None:
@@ -86,7 +74,7 @@ class TestProductView(TestCase):
             "name": secrets.token_hex(16),
             "cost": "{:.2f}".format(secrets.randbelow(1000)),
         }
-        response = self.client.put(
+        response: Any = self.client.put(
             f"{self.path}{saved_product.id}/",
             data=new_product,
             content_type="application/json",
@@ -101,16 +89,16 @@ class TestProductView(TestCase):
             "name": secrets.token_hex(16),
             "cost": "{:.2f}".format(secrets.randbelow(1000)),
         }
-        response = self.client.put(f"{self.path}1/", data=new_product, content_type="application/json")
+        response: Any = self.client.put(f"{self.path}99999/", data=new_product, content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_product_should_pass(self) -> None:
         """Test delete product with valid request."""
         saved_product: Product = save_product()
-        response = self.client.delete(f"{self.path}{saved_product.id}/")
+        response: Any = self.client.delete(f"{self.path}{saved_product.id}/")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_product_should_fail_when_id_is_not_found(self) -> None:
         """Test delete product with invalid request where product id does not exist."""
-        response = self.client.delete(f"{self.path}1/")
+        response: Any = self.client.delete(f"{self.path}99999/")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
