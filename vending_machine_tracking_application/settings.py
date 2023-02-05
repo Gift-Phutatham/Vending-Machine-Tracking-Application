394ade1839c505bb9182fe705cd06d3bdc101412
Sujinna Phutatham <gift@Sujinnas-MacBook-Pro.local>
2023-02-05 18:46:54 +0700

    fix ci

--- vending_machine_tracking_application/settings.py
+++ vending_machine_tracking_application/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": "mydatabase",
         "USER": "mydatabaseuser",
         "PASSWORD": "mypassword",
-        "HOST": "127.0.0.1",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
